d4260b76ccc95e25a8b2c4b423507d673e9268ff
el-rey08 <hammedlanre71@gmail.com>
2024-09-04 11:06:25 +0100

    typo

--- router/attendanceRouter.js
+++ router/attendanceRouter.js
@@ -2,5 +2,5 @@ const express = require('express')
 const { authenticate, checkAdminOrTeacher } = require('../middleware/auth')
 const { takeAttendance } = require('../controller/attendanceController')
 const router = express.Router()
-router.post('/attendance',authenticate,checkAdminOrTeacher,takeAttendance)
+router.post('/mark-attendance',authenticate,checkAdminOrTeacher,takeAttendance)
 module.exports= router
